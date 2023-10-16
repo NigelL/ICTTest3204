f6b3e8cc70e44b2d502db52e562ed386be488ef0
ooinic <ooinic@gmail.com>
2023-10-16 20:39:25 +0800

    pain

--- publichtml/index.php
+++ publichtml/index.php
@@ -21,7 +21,7 @@
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-	$target_dir = "uploads/";
+	$target_dir = "upload/";
 	$target_file = $target_dir . basename($_FILES["file"]["name"]);
 	$uploadOk = 1;
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -34,7 +34,7 @@ if(isset($_POST["submit"])) {
     
     if($uploadOk == 1){
         move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-        echo "File uploaded /uploads/".$_FILES["file"]["name"];
+        echo "File uploaded /upload/".$_FILES["file"]["name"];
     }
 }
 ?>
